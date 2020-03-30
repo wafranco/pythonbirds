e62575fd271b879a5b8748c409c87f8ccff2af96
wafranco <261213@mor>
2020-03-30 20:38:50 -0300

    criação de métodos e atributos de classe

---
+++ oo/carro.py
@@ -0,0 +1,70 @@
+class Motor:
+    def __init__(self, velocidade=0):
+        self.velocidade = velocidade
+
+    def acelerar(self):
+        self.velocidade += 1
+        return self.velocidade
+
+    def frear(self):
+        self.velocidade = 0 if self.velocidade - 2 < 0 else self.velocidade - 2
+
+
+if __name__ == '__main__':
+    motor = Motor(velocidade=0)
+    print(motor.velocidade)
+    motor.acelerar()
+    print(motor.velocidade)
+    motor.acelerar()
+    print(motor.velocidade)
+    motor.frear()
+    print(motor.velocidade)
+    motor.frear()
+    print(motor.velocidade)
+
+    # direcao:
+
+
+class Direcao():
+    def __init__(self):
+        self.direcao = 'Norte'
+
+    def girar_a_direita(self):
+        if self.direcao == 'Norte':
+            self.direcao = 'Leste'
+        elif self.direcao == 'Leste':
+            self.direcao = 'Sul'
+        elif self.direcao == 'Sul':
+            self.direcao = 'Oeste'
+        elif self.direcao == 'Oeste':
+            self.direcao = 'Norte'
+
+    def girar_a_esquerda(self):
+        if self.direcao == 'Norte':
+            self.direcao = 'Oeste'
+        elif self.direcao == 'Oeste':
+            self.direcao = 'Sul'
+        elif self.direcao == 'Sul':
+            self.direcao = 'Leste'
+        elif self.direcao == 'Leste':
+            self.direcao = 'Norte'
+
+if __name__ == '__main__':
+    direcao = Direcao()
+    print(direcao.direcao)
+    direcao.girar_a_direita()
+    print(direcao.direcao)
+    direcao.girar_a_direita()
+    print(direcao.direcao)
+    direcao.girar_a_direita()
+    print(direcao.direcao)
+    direcao.girar_a_direita()
+    print(direcao.direcao)
+    direcao.girar_a_esquerda()
+    print(direcao.direcao)
+    direcao.girar_a_esquerda()
+    print(direcao.direcao)
+    direcao.girar_a_esquerda()
+    print(direcao.direcao)
+    direcao.girar_a_esquerda()
+    print(direcao.direcao)
