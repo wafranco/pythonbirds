3b5a5d9296aac48f3c416a2f7ff4fb8e3b3241e1
wafranco <261213@mor>
2020-03-30 20:40:32 -0300

    criação da rotina de exercicio simulando um carro, com classes motor e direcao e metodos acelerar, frear, girar_a_direita e girar_a_esquerda

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,23 @@
+"""
+Você deve criar uma classe carro que vai possuir
+dois atributos compostos por outras duas classes:
+
+Motor
+Direção
+O Motor terá a responsabilidade de controlar a velocidade.
+Ele oferece os seguintes atributos:
+
+Atributo de dado velocidade
+Método acelerar, que deverá incremetar a velocidade de uma unidade
+Método frear que deverá decrementar a velocidade em duas unidades
+A Direção terá a responsabilidade de controlar a direção. Ela oferece
+os seguintes atributos:
+
+Valor de diração com valores possíveis: Norte, Sul, Leste, Oeste.
+Método girar_a_direita
+Método girar_a_esquerda
+
+"""
 class Motor:
     def __init__(self, velocidade=0):
         self.velocidade = velocidade
