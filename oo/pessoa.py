a5cda0eda71880bee9ebc1b83faaa8ea2e9d2be2
wafranco <261213@mor>
2020-03-29 21:49:32 -0300

    criação de métodos e atributos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,9 +8,18 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 32
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     walter = Pessoa(nome='Walter Franco', idade=54)
     mozart = Pessoa(walter, nome='Mozart', idade=90)
+    print(Pessoa.cumprimentar(mozart))
     print (mozart.nome)
     print (mozart.idade)
     for filho in mozart.filhos:
@@ -26,6 +35,10 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(mozart.olhos)
     print(walter.olhos)
+    print(id(Pessoa.olhos), id(mozart.olhos), id(walter.olhos))
+    print(Pessoa.metodo_estatico(), mozart.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), mozart.nome_e_atributos_de_classe())
+
 
 #    p = Pessoa('Patrícia')
 #    print(Pessoa.cumprimentar(p))
