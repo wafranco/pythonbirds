c050b2d547ad42f6f921662410d679375f3f1701
wafranco <261213@mor>
2020-03-30 21:40:29 -0300

    correção do exercício, adicionado o dicionário para eliminação de vários if/elif

--- oo/carro.py
+++ oo/carro.py
@@ -17,21 +17,97 @@ Valor de diração com valores possíveis: Norte, Sul, Leste, Oeste.
 Método girar_a_direita
 Método girar_a_esquerda
 
+Exemplo
+    # >>> testando motor
+    >>> motor = Motor()
+    >>> motor.velocidade
+    0
+    >>> motor = Motor()
+    >>> motor.velocidade
+    1
+    >>> motor = Motor()
+    >>> motor.velocidade
+    2
+    >>> motor = Motor()
+    >>> motor.velocidade
+    3
+    >>> motor = Motor()
+    >>> motor.frear
+    1
+    >>> motor = Motor()
+    >>> motor.frear
+    0
+    # >>> testando direcao
+    >>> direcao = Direcao()
+    >>> direcao.valor
+    ‘Norte’
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    ‘Sul’
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    ‘Oeste’
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    ‘Norte’
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    ‘Oeste’
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    ‘Sul’
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    ‘Leste’
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    ‘Norte’
+    >>> carro = Carro(direcao, motor)
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    1
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    2
+    >>> carro.frear()
+    >>> carro.calcular_velocidade()
+    0
+    >>>carro.calcular_direcao()
+    ‘Norte’
+    >>>carro.girar_a_direita()
+    >>>carro.calcular_direcao()
+    ‘Leste’
+    >>>carro.girar_a_esquerda()
+    >>>carro.calcular_direcao()
+    ‘Norte’
+    >>>carro.girar_a_esquerda()
+    >>>carro.calcular_direcao()
+    ‘Oeste’
+
 """
+
+NORTE = 'Norte'
+LESTE = 'Leste'
+SUL = 'Sul'
+OESTE = 'Oeste'
+
+
 class Motor:
-    def __init__(self, velocidade=0):
-        self.velocidade = velocidade
+    def __init__(self):
+        self.velocidade = 0
 
     def acelerar(self):
         self.velocidade += 1
-        return self.velocidade
 
     def frear(self):
-        self.velocidade = 0 if self.velocidade - 2 < 0 else self.velocidade - 2
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
 
 
 if __name__ == '__main__':
-    motor = Motor(velocidade=0)
+    motor = Motor()
     print(motor.velocidade)
     motor.acelerar()
     print(motor.velocidade)
@@ -46,28 +122,40 @@ if __name__ == '__main__':
 
 
 class Direcao():
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+
     def __init__(self):
         self.direcao = 'Norte'
 
     def girar_a_direita(self):
-        if self.direcao == 'Norte':
-            self.direcao = 'Leste'
-        elif self.direcao == 'Leste':
-            self.direcao = 'Sul'
-        elif self.direcao == 'Sul':
-            self.direcao = 'Oeste'
-        elif self.direcao == 'Oeste':
-            self.direcao = 'Norte'
+        self.direcao = self.rotacao_a_direita_dct[self.direcao]
+
+    #        if self.direcao == 'Norte':
+    #            self.direcao = 'Leste'
+    #        elif self.direcao == 'Leste':
+    #            self.direcao = 'Sul'
+    #        elif self.direcao == 'Sul':
+    #            self.direcao = 'Oeste'
+    #        elif self.direcao == 'Oeste':
+    #            self.direcao = 'Norte'
 
     def girar_a_esquerda(self):
-        if self.direcao == 'Norte':
-            self.direcao = 'Oeste'
-        elif self.direcao == 'Oeste':
-            self.direcao = 'Sul'
-        elif self.direcao == 'Sul':
-            self.direcao = 'Leste'
-        elif self.direcao == 'Leste':
-            self.direcao = 'Norte'
+        self.direcao = self.rotacao_a_esquerda_dct[self.direcao]
+
+
+#        if self.direcao == 'Norte':
+#            self.direcao = 'Oeste'
+#        elif self.direcao == 'Oeste':
+#            self.direcao = 'Sul'
+#        elif self.direcao == 'Sul':
+#            self.direcao = 'Leste'
+#        elif self.direcao == 'Leste':
+#            self.direcao = 'Norte'
 
 if __name__ == '__main__':
     direcao = Direcao()
